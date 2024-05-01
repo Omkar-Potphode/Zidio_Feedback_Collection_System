1095cdef00988c0f54a82f0f62190af45c101d96
Omkar Potphode <omkar03potphode@gmail.com>
2024-05-02 02:15:59 +0530

    ✨ Added the Hero Section

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -22,6 +22,15 @@ export default {
         code: "var(--font-code)",
         grotesk: "var(--font-grotesk)",
       },
+      animation: {
+        grid: "grid 15s linear infinite",
+      },
+      keyframes: {
+        grid: {
+          "0%": { transform: "translateY(-50%)" },
+          "100%": { transform: "translateY(0)" },
+        },
+      },
     },
   },
   plugins: [],
